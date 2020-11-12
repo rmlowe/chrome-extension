d6c308736428172ce2599b73e05655507f0382c1
Robert Lowe <robertmlowe@rmlowe.com>
2020-11-12 17:03:09 +0800

    Opening a new tab

--- my-cool-extension/content.js
+++ my-cool-extension/content.js
@@ -5,6 +5,9 @@ chrome.runtime.onMessage.addListener(
       var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       console.log(firstHref);
+
+      // This line is new!
+      chrome.runtime.sendMessage({ "message": "open_new_tab", "url": firstHref });
     }
   }
 );
